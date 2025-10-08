b577026997e695668c3385720455126b7cb8047a
obexx <leightontegland7@gmail.com>
2025-10-07 21:30:55 -0400

    made scale factor bias macos exclusive

--- helmer-engine/src/runtime/runtime.rs
+++ helmer-engine/src/runtime/runtime.rs
@@ -594,7 +594,11 @@ impl ApplicationHandler for Runtime {
 
                 let mut input = self.input_manager.write();
                 input.window_size = UVec2::new(new_size.width, new_size.height);
-                input.scale_factor = self.window.as_ref().unwrap().scale_factor() / 1.2;
+                input.scale_factor = if cfg!(target_os = "macos") {
+                    self.window.as_ref().unwrap().scale_factor() / 1.2
+                } else {
+                    self.window.as_ref().unwrap().scale_factor()
+                };
 
                 if new_size.width > 0 && new_size.height > 0 {
                     let mut ecs_guard = self.ecs.write();

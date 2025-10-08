dc00095c0742ebf3190db9c7febe3947380ecdcc
obexx <leightontegland7@gmail.com>
2025-10-07 22:11:52 -0400

    helmer editor foundation

--- helmer-editor/src/main.rs
+++ helmer-editor/src/main.rs
@@ -1,3 +1,43 @@
+use std::{any::TypeId, collections::HashSet, env};
+
+use helmer_editor::systems::interaction::freecam::FreecamSystem;
+use helmer_engine::{provided::components::{ActiveCamera, Camera, Transform}, runtime::runtime::Runtime};
+
 fn main() {
-    println!("Hello, world!");
+    let current_path = env::current_dir().expect("Failed to find executable path");
+    if current_path.ends_with("helmer-rs") {
+        env::set_current_dir(current_path.join("helmer-editor"))
+            .expect("Failed to change working directory");
+    }
+
+    let mut runtime = Runtime::new(|app| {
+        let mut ecs_guard = app.ecs.write();
+
+        ecs_guard.system_scheduler.register_system(
+            FreecamSystem::new(1.0, 0.5),
+            30,
+            vec![],
+            HashSet::from([TypeId::of::<Transform>()]),
+            HashSet::from([TypeId::of::<Transform>()]),
+        );
+
+        let camera_entity = ecs_guard.create_entity();
+        ecs_guard.add_component(
+            camera_entity,
+            Transform {
+                position: glam::Vec3::new(0.0, 0.0, -3.0),
+                rotation: glam::Quat::IDENTITY,
+                scale: glam::Vec3::ONE,
+            },
+        );
+        ecs_guard.add_component(
+            camera_entity,
+            Camera {
+                far_plane: 300.0,
+                ..Default::default()
+            },
+        );
+        ecs_guard.add_component(camera_entity, ActiveCamera {});
+    });
+    runtime.init();
 }

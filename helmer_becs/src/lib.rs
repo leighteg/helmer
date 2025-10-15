f9f884b0aaf43403f3bd9f00c5c094d0d4a1ba99
obexx <leightontegland7@gmail.com>
2025-10-15 10:00:51 -0400

    helmer_becs foundation

--- helmer_becs/src/lib.rs
+++ helmer_becs/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use bevy_ecs::{schedule::Schedule, world::World};
+use helmer::runtime::{asset_server::AssetServer, runtime::Runtime};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn helmer_ecs_init(init_callback: fn(&mut World, &mut Schedule, &AssetServer)) {
+    let world = World::new();
+    let scheduler = Schedule::default();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let mut runtime = Runtime::new(
+        (world, scheduler),
+        move |runtime, (world, scheduler)| {
+            init_callback(
+                world,
+                scheduler,
+                &runtime.asset_server.as_ref().unwrap().lock(),
+            );
+        },
+        |dt, input_manager, (world, schedule)| (None, None),
+        |new_size, (world, schedule)| {},
+    );
+    runtime.init();
 }

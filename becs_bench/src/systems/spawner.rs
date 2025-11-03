86de55ea55523fa24f245210ef3627443958c6aa
obexx <leightontegland7@gmail.com>
2025-11-03 01:41:42 -0500

    improved becs_bench spawner system

--- becs_bench/src/systems/spawner.rs
+++ becs_bench/src/systems/spawner.rs
@@ -19,6 +19,7 @@ pub struct MeshRendererStore {
 
 #[derive(Resource, Default)]
 pub struct SpawnerSystemResource {
+    pub max_entities: usize,
     pub spawn_iters: usize,
     pub spawned_entities: HashSet<Entity>,
     pub despawn_all: bool,
@@ -30,16 +31,64 @@ pub fn spawner_system(
     mut spawner_system_resource: ResMut<SpawnerSystemResource>,
     mut egui_res: ResMut<EguiResource>,
 ) {
+    // --- UI ---
+    egui_res.windows.push((
+        Box::new(move |ui, world, _input_arc| {
+            if let Some(mut spawner_system_resource) =
+                world.get_resource_mut::<SpawnerSystemResource>()
+            {
+                ui.label(format!(
+                    "spawned entity count: {}",
+                    spawner_system_resource.spawned_entities.len()
+                ));
+                ui.add(
+                    egui::DragValue::new(&mut spawner_system_resource.max_entities)
+                        .prefix("max entities: "),
+                );
+                ui.add(
+                    egui::DragValue::new(&mut spawner_system_resource.spawn_iters)
+                        .prefix("spawn iters: "),
+                );
+
+                if ui.button("destroy spawned entities").clicked() {
+                    spawner_system_resource.despawn_all = true;
+                }
+            }
+        }),
+        "spawn system".to_string(),
+    ));
+
+    // --- ECS logic ---
+
     if spawner_system_resource.despawn_all {
         for entity in spawner_system_resource.spawned_entities.iter() {
             commands.entity(*entity).despawn();
         }
+        spawner_system_resource.spawned_entities.clear();
         spawner_system_resource.despawn_all = false;
     }
 
-    /*if spawner_system_resource.spawned_count > 3000 {
+    if spawner_system_resource.spawned_entities.len() > spawner_system_resource.max_entities {
+        let mut ran = false;
+        let mut entity_to_remove: Option<Entity> = None;
+        for entity in spawner_system_resource.spawned_entities.iter() {
+            if ran {
+                break;
+            }
+
+            commands.entity(*entity).despawn();
+            entity_to_remove = Some(*entity);
+            ran = true;
+        }
+
+        if let Some(entity_to_remove) = entity_to_remove {
+            spawner_system_resource
+                .spawned_entities
+                .remove(&entity_to_remove);
+        }
+
         return;
-    }*/
+    }
 
     if let Some(mesh_renderer) = mesh_renderer_store.mesh_renderers.get("default") {
         for i in 0..spawner_system_resource.spawn_iters {
@@ -56,27 +105,4 @@ pub fn spawner_system(
                 .insert(new_entity.id());
         }
     }
-
-    // --- UI ---
-    egui_res.windows.push((
-        Box::new(move |ui, world, _input_arc| {
-            if let Some(mut spawner_system_resource) =
-                world.get_resource_mut::<SpawnerSystemResource>()
-            {
-                ui.label(format!(
-                    "spawned entity count: {}",
-                    spawner_system_resource.spawned_entities.len()
-                ));
-                ui.add(
-                    egui::DragValue::new(&mut spawner_system_resource.spawn_iters)
-                        .prefix("spawn iters: "),
-                );
-
-                if ui.button("destroy spawned entities").clicked() {
-                    spawner_system_resource.despawn_all = true;
-                }
-            }
-        }),
-        "spawn system".to_string(),
-    ));
 }
